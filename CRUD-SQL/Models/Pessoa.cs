7cdced74de3643e8fb388e67c9e60291323c09d6
Felippe <felippe_nvieira@hotmail.com>
2022-01-11 15:17:19 -0300

    Created the Pessoa Model

---
+++ CRUD-SQL/Models/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_SQL.Models
+{
+    public class Pessoa
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public string CPF { get; set; }
+        public string MaritalStatus { get; set; }
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Address { get; set; }
+    }
+}
